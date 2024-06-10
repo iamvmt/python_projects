1ee911da89f02af257efd45c87c6f87bf85a2ab4
iamvmt <138132670+iamvmt@users.noreply.github.com>
2024-06-10 09:03:18 +0530

    Add files via upload
    In this, we use try and except in our function to optimize them.

---
+++ Lec58TryExcept.py
@@ -0,0 +1,12 @@
+# working of try() 
+def divide(x, y):
+	try:
+		# Floor Division : Gives only Fractional Part as Answer
+		result = x // y
+		print("Yeah ! Your answer is :", result)
+	except ZeroDivisionError:
+		print("Sorry ! You are dividing by zero ")
+
+# Look at parameters and note the working of Program
+divide(3, 2)
+divide(5, 0)
